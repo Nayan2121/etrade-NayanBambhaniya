f74ac61e7e22d0713f755afe154b55436a743747
darshan <darshanchauhan5064@gmail.com>
2023-10-11 15:44:37 +0530

    11-10-23

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,28 +1,55 @@
-import React from 'react' 
- 
- 
-export const NavBar = () => { 
-    return ( 
-        <div className='flex justify-between px-10 bg-white rounded-md py-4 items-center hover:shadow-md'> 
-            <div> 
+import React from 'react'
+import '../Navbar/NavBar.css'
+
+export const NavBar = () => {
+    return (
+        <div className='flex justify-between px-10 bg-white rounded-md items-center hover:shadow-md'>
+            <div>
                 <img src="./src/assets/Home/Logo.png" alt="" />
-            </div> 
- 
-            <div className=' flex justify-between' style={{maxWidth}}> 
-               <button className='font-semibold'>Home <i class="fa-solid fa-angle-down text-gray-500 transition-all text-xs"></i></button>  
-               <button className='font-semibold'>Shop <i class="fa-solid fa-angle-down text-gray-500 transition-all text-xs"></i></button>  
-               <button className='font-semibold'>Pages <i class="fa-solid fa-angle-down text-gray-500 transition-all text-xs"></i></button>  
-               <button className='font-semibold'>About</button>  
-               <button className='font-semibold'>Blog <i class="fa-solid fa-angle-down text-gray-500 transition-all text-xs"></i></button>  
-               <button className='font-semibold'>Contact</button>  
-            </div> 
-             
-            <div className='text-2xl text-gray-600 flex w-[180px] justify-between'> 
-                <span className='border h-10 w-10 flex justify-center items-center rounded-full hover:bg-pink-600 transition duration-500'><i class="ri-search-line  hover:bg-pink-600 rounded-full"></i></span> 
-                <span className='border h-10 w-10 flex justify-center items-center rounded-full hover:bg-pink-600 transition duration-500'><i class="ri-heart-line hover:bg-pink-600 rounded-full"></i></span> 
-                <span className='border h-10 w-10 flex justify-center items-center rounded-full hover:bg-pink-600 transition duration-500'><i class="ri-shopping-cart-line hover:bg-pink-600 rounded-full"></i></span> 
-                <span className='border h-10 w-10 flex justify-center items-center rounded-full hover:bg-pink-600 transition duration-500'><i class="ri-user-3-line hover:bg-pink-600 rounded-full"></i></span> 
-            </div> 
-        </div> 
+            </div>
+
+
+            <div className='navbar flex justify-between p-0 m-0'>
+                <div className='home-dropdown'>
+                    <button className='home-button underline decoration-[3px] underline-offset-4 h-24'>Home<i class="fa-solid fa-angle-down text-gray-500 transition-all text-xs ml-1"></i></button>
+                    <div className='home-content'>
+                        <ul className='list bg-white rounded-md'>
+                            <li><a href=""></a>Home-Electronics</li>
+                            <li><a href=""></a>Home-NFT</li>
+                            <li><a href=""></a>Home-NFT</li>
+                            <li><a href=""></a>Home-NFT</li>
+                            <li><a href=""></a>Home-NFT</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='home-dropdown'>
+                    <button className='home-button underline decoration-[3px] underline-offset-4'>Home<i class=" fa-solid fa-angle-down text-gray-500 transition-all text-xs ml-1"></i></button>
+                    <div className='home-content'>
+                        <ul className='list'>
+                            <li><a href=""></a>Home-Electronics</li>
+                            <li><a href=""></a>Home-NFT</li>
+                            <li><a href=""></a>Home-NFT</li>
+                            <li><a href=""></a>Home-NFT</li>
+                            <li><a href=""></a>Home-NFT</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className='text-2xl text-gray-600 flex justify-between w-[160px] h-10'>
+                <ul className='flex justify-between w-[20px] btn-hover'>
+                    <li className='nav-icon h-10 w-10 flex justify-center items-center rounded-full'><i class="ri-search-line hover:text-slate-100"></i></li>
+                </ul>
+                <ul className='flex justify-between w-[20px] btn-hover'>
+                    <li className='nav-icon h-10 w-10 flex justify-center items-center rounded-full'><i class="ri-heart-line hover:text-slate-100"></i></li>
+                </ul>
+                <ul className='flex justify-between w-[20px] btn-hover'>
+                    <li className='nav-icon h-10 w-10 flex justify-center items-center rounded-full'><i class="ri-shopping-cart-line hover:text-slate-100"></i></li>
+                </ul>
+                <ul className='flex justify-between w-[20px] btn-hover'>
+                    <li className='nav-icon h-10 w-10 flex justify-center items-center rounded-full'><i class="ri-user-3-line hover:text-slate-100"></i></li>
+                </ul>
+            </div>
+        </div>
     )
 }
